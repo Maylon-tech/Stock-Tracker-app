9e8b2b7de928435599a1714e1a2862c83e599d58
Maylon-tech <minatokiroisen77@gmail.com>
2023-11-16 16:11:26 +0900

    create a layout html css

---
+++ main.js
@@ -0,0 +1,8 @@
+const symbolInput = document.querySelector('#symbol')
+const stockList = document.querySelector('#stock-list')
+
+//  Function to Fetch and Display the top 10 stocks
+function fetchTopStocks() {
+    // Fetching data from API
+    fetch('')
+}
